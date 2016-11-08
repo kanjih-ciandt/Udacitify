59661619b055b6d405bc9c6f65d9d4852899eaa6
kanjih <kanjih@ciandt.com>
2016-11-08 00:04:48 -0200

    Initial Version

---
+++ app/src/main/java/com/udacity/udacitify/StartActivity.java
@@ -0,0 +1,20 @@
+package com.udacity.udacitify;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_start);
+    }
+}
