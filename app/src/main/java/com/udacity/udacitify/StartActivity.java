30863c7579c2ff028458f06328b101429c8d148c
kanjih <kanjih@ciandt.com>
2016-11-10 02:14:20 -0200

    implemented menus

--- app/src/main/java/com/udacity/udacitify/StartActivity.java
+++ app/src/main/java/com/udacity/udacitify/StartActivity.java
@@ -1,6 +1,7 @@
 package com.udacity.udacitify;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,13 +9,31 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_start);
+
+        Button btnSignUp =  (Button) findViewById(R.id.btn_signup);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, SignUpActivity.class));
+            }
+        });
+
+        Button btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, MainActivity.class));
+            }
+        });
+
     }
 }
