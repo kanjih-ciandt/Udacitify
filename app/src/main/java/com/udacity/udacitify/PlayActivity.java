6b75346a6552605603dfdee51364b1c56b44c95c
kanjih <kanjih@ciandt.com>
2016-11-17 22:57:27 -0200

    settings UI done

--- app/src/main/java/com/udacity/udacitify/PlayActivity.java
+++ app/src/main/java/com/udacity/udacitify/PlayActivity.java
@@ -3,6 +3,14 @@ package com.udacity.udacitify;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *  Class responsible for play the music
+ *
+ *  This class will use Android multimidia framework
+ *  (https://developer.android.com/guide/topics/media/mediaplayer.html)
+ *
+ *
+ */
 public class PlayActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/udacity/udacitify/payment/PaymentBusinessServices.java
@@ -0,0 +1,20 @@
+package com.udacity.udacitify.payment;
+
+/**
+ * Created by kneto on 11/17/16.
+ *
+ * This class is responsible for payment funcionality.
+ *
+ * IMPORTANT:
+ * All business complexity will be there in backend part of application.
+ * (The Backend will be developed using Google Cloud Platform - App Engine Standalone Version
+ * {https://cloud.google.com/appengine/} )
+ *
+ *The backend will use VISA CHECKOUT. Visa Checkout use REST API for Payment.
+ *
+ *
+ *
+ */
+
+public class PaymentBusinessServices {
+}
